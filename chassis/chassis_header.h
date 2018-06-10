25829b990e6ee80ce200441a18a17bada1389df1
iamtaegu <gmdals45@gmail.com>
2018-06-10 09:48:29 +0900

    wiringPi was applied to chassis

---
+++ chassis/chassis_header.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <pigpiod_if2.h>
+#include <unistd.h>
+ 
+#define IN1 24 // GPIO19 
+#define IN2 23 // GPIO13
+#define IN3 21 // GPIO5
+#define IN4 22 // GPIO6
+
+void backTurnL (float);
+void turnL (float);
+void backTurnR(float);
+void turnR(float);
+
+void goForward (float);
+void goBackward (int,int, float);
+
+void getItem_1();
+void getItem_2();
+void getItem_3();
+
+void backFrom_1();
+void backFrom_2();
+void backFrom_3();
+
+int ChassisMove();
+void* ChassisThreadRun (void *);
+void init_chassis_thread();
+
+bool chassisThreadFlag;
+unsigned char chassisOption;
