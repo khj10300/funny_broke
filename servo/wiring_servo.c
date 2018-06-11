1bd354097965f6a6cc9f0ba48b50b3e2696128e5
khj10300 <khj10300@github.com>
2018-06-11 15:06:43 +0900

    led

--- servo/wiring_servo.c
+++ servo/wiring_servo.c
@@ -1,92 +1,50 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <wiringPi.h>
-#include <softPwm.h> 
-#include <pigpiod_if2.h>
+#include "test_header.h"
 
-typedef enum {false, true} bool; 
-
-#define RIGHT_SERVO 29
-#define LEFT_SERVO 28
-#define FRONT_SERVO 27
-#define BACK_SERVO 26
-int servoMove(int);
-void* servoThreadRun (void *);
-void init_servo_thread();
-
-bool servoThreadFlag;
-int servoOption;
-
-int pi; 
-void InitPigpiod ();
-int ServoMovePigpiod(int gpio, int degree);
-
+int front_speed = 7;
+int back_speed = 7;
 
 int servoMove(int op)
 {
-    switch (op)
+    if (!(op == 1 || op == 2 || op == 3 || op == 4))
     {
-        case 1: softPwmWrite(BACK_SERVO, 7);  time_sleep(1);
-                softPwmWrite(FRONT_SERVO,12); break; // upwards moving.  
-        case 2: softPwmWrite(FRONT_SERVO, 7); time_sleep(1); 
-                softPwmWrite(BACK_SERVO,12); break; // downward moving.  
-        case 3: softPwmWrite(RIGHT_SERVO, 7); time_sleep(1);
-                softPwmWrite(LEFT_SERVO,12); break; // leftward moving.  
-        case 4: softPwmWrite(LEFT_SERVO, 7); time_sleep(1);
-                softPwmWrite(RIGHT_SERVO,12); break; // rightward moving.            
+        fprintf (stderr, "invalid option number taek either 1 or 2\n");
+        return -1;
     }
-    return 1;
-}
 
-
-
-int main()
-{
-    /*
-    for (;;)
-    {
-     int op;
-     scanf ("%d", &op);
-     printf ("option : %d\n", op);
-     servoMove(op);
-     memset (&op, 0, sizeof(int));
-    }
-    */
-    InitPigpiod();
-
-    while (1)
-    {
-        int op=0;
-        scanf ("%d", &op);
-        printf ("op : %d\n", op);
-        switch (op)
-        {
-            case 1: ServoMovePigpiod (BACK_SERVO, 0); time_sleep(1);
-                    ServoMovePigpiod (FRONT_SERVO, 90); break;
-            case 2: ServoMovePigpiod (FRONT_SERVO, 0); time_sleep (1);
-                    ServoMovePigpiod (BACK_SERVO, 90); break;
-                    
-            case 3: ServoMovePigpiod (RIGHT_SERVO, 0); time_sleep(1);
-                    ServoMovePigpiod (LEFT_SERVO, 90); break;
-
-            case 4: ServoMovePigpiod (LEFT_SERVO, 0); time_sleep (1);
-                    ServoMovePigpiod (RIGHT_SERVO, 90); break;        
-        }
-    }
-    pigpio_stop(pi); 
-
-}
-
-void* servoThreadRun (void *data)
-{
-    if(wiringPiSetup()==-1)
+     if(wiringPiSetup()==-1)
         return -1;
     softPwmCreate(RIGHT_SERVO, 0, 200);
     softPwmCreate(LEFT_SERVO, 0, 200);
     softPwmCreate(FRONT_SERVO, 0, 200);
     softPwmCreate(BACK_SERVO, 0, 200);
-    
+
+    switch (op)
+    {
+        case 1: 
+              //softPwmWrite(BACK_SERVO, 7);  time_sleep(1);
+                softPwmWrite(FRONT_SERVO,10); 
+                break; // upwards moving.  
+        case 2: 
+                softPwmWrite(FRONT_SERVO, 16);  time_sleep(1); 
+                //softPwmWrite(BACK_SERVO, back_speed); 
+                time_sleep(1); 
+                break; // downward moving.  
+        case 3:
+                softPwmWrite(RIGHT_SERVO, 8); //time_sleep(1);
+                //softPwmWrite(LEFT_SERVO, 48);
+                time_sleep(1); 
+                break; // leftward moving.  
+        case 4: 
+             //   softPwmWrite(LEFT_SERVO, 24); time_sleep(1);
+                softPwmWrite(RIGHT_SERVO, 16);
+                time_sleep(1); 
+                break; // rightward moving.            
+    }
+    return 1;
+}
+
+void* servoThreadRun (void *data)
+{
     servoThreadFlag = false;
     while (1) 
     {
@@ -114,33 +72,5 @@ void init_servo_thread()
         fprintf (stderr, "servo pthread_create error\n");
         exit(-1);
     }
-}
-
-int ServoMovePigpiod(int gpio, int degree) 
-{ 
-    int pulse_width; 
-    
-    if(degree >= 0 && degree <= 180)
-    { 
-        pulse_width = (degree * 11.11111) + 500; 
-        set_servo_pulsewidth(pi, gpio, pulse_width); 
-        printf("degree: %d\n", degree); 
-    } 
-    return 0; 
-}
-
-void
-InitPigpiod ()
-{
-    if((pi = pigpio_start(NULL, NULL)) < 0)
-    { 
-        fprintf(stderr, "pigpio_start error\n"); 
-        return 1; 
-    }
-    set_mode(pi, RIGHT_SERVO, PI_OUTPUT);  
-    set_mode(pi, LEFT_SERVO, PI_OUTPUT); 
-    set_mode(pi, FRONT_SERVO, PI_OUTPUT); 
-    set_mode(pi, BACK_SERVO, PI_OUTPUT); 
-
 }
 
